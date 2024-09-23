4f12d07c95029b7806f061437724226f6b578d9d
mruiz-ur <espartanmanu1122@gmail.com>
2024-09-23 12:08:08 +0200

    updated function memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mruiz-ur <mruiz-ur@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 12:46:33 by mruiz-ur          #+#    #+#             */
-/*   Updated: 2024/09/19 17:59:57 by mruiz-ur         ###   ########.fr       */
+/*   Updated: 2024/09/23 11:44:14 by mruiz-ur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,46 +14,22 @@
 
 void *ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t i;
+	// size_t i;
 	char* a_dest;
 	char* b_src;
 
-	i = ft_strlen(dest);
+	// i = ft_strlen(dest);
 	b_src = (char*)src;
 	a_dest = (char*)dest;
 	if(a_dest == b_src)
 		return (dest);	
-	if(a_dest > b_src)
-		ft_memcpy(dest, src, n);
-	else
+	if((a_dest > b_src) && (b_src + n) > a_dest)
 	{
-		while(a_dest[i] && b_src[i])
-		{
-			b_src[i] = a_dest[i];
-			i--;
-		}
+		while (n--)
+			b_src = a_dest;
+		return (dest);
 	}
-	return (dest);	
-}
-
-
-void *ft_memcpy(void *dest, const void *src, unsigned int n)
-{
-    size_t i;
-    unsigned const char* a;
-    unsigned char* b;
-
-    b = dest;
-    a = src;
-    i = 0;
-    while(b[i] != '\0' && i < n)
-    {
-        b[i] = a[i];
-        write(1, &b[i], 1);
-        i++;
-    }
-    b[i] = '\0';
-
+	return (ft_memcpy(dest, src, n));	
 }
 
 int	main(void)
