966c15c3133f1944eccca85e7abb452772aba66e
mruiz-ur <espartanmanu1122@gmail.com>
2024-09-23 19:02:27 +0200

    updated function atoi

---
+++ ft_atoi.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mruiz-ur <mruiz-ur@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/23 17:54:16 by mruiz-ur          #+#    #+#             */
+/*   Updated: 2024/09/23 19:02:04 by mruiz-ur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+
+int	ft_atoi(const char* str)
+{
+	size_t i;
+	int	n;
+	int sign;
+
+	n = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\v' || str[i] == '\n' 
+		|| str[i] == '\r' || str[i] == '\f')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		n = (n * 10) + (str[i] - '0');
+		i++;	
+	}	
+	return (n * sign);	
+}
+
+// int main()
+// {
+// 	char str[] = "-42hola";
+// 	int i = ft_atoi(str);
+// 	printf("%d\n", i);
+// 	char number[] = "-42hola";
+// 	int b = atoi(number);
+// 	printf("%d\n", b);
+// }
