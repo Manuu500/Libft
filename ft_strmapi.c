75a1f63e8fb7e19791d6cb026ee1b38ef1bd0861
mruiz-ur <espartanmanu1122@gmail.com>
2024-10-07 18:12:19 +0200

    added function

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -12,7 +12,35 @@
 
 #include "libft.h"
 
+char    ft_up (unsigned int *i, char str)
+{
+    if (str >= 97 && str <= 122)
+        str -= 32;
+    return (str);
+}
+
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-    
+    size_t i;
+    char *str;    
+
+    i = 0;
+    if (!s || !f)
+        return (0);
+    str = malloc(sizeof(char) * ft_strlen(s) + 1);
+    if (!str)
+        return (0);
+    while (s[i] != '\0')
+    {
+        str[i] = f(i, s[i]);
+        i++;
+    }
+    str[i] = '\0';
+    return (str);
+}
+
+int main (void)
+{
+    char *i = ft_strmapi("HOLA", ft_up);
+    printf("%s\n", i);
 }
