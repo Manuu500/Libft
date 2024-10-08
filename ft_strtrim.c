6b3d9e0e49185f3e2059da6fb27fabc7a345bd10
mruiz-ur <espartanmanu1122@gmail.com>
2024-10-08 17:14:37 +0200

    updated function

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mruiz-ur <mruiz-ur@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 19:03:30 by mruiz-ur          #+#    #+#             */
-/*   Updated: 2024/10/08 17:07:52 by mruiz-ur         ###   ########.fr       */
+/*   Updated: 2024/10/08 17:13:09 by mruiz-ur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,16 @@ char    *ft_strtrim(char const *s1, char const *set)
         return (ft_strdup(s1));
 
     k = 0;
-    j = ft_strlen(s1) - 1;
+    j = ft_strlen(s1);
     i = 0;
     while (s1[i] != '\0' && ft_strchr(set, s1[i]))
         i++;
-    while (j > i && ft_strchr(set, s1[j]))
+    while (j > i && ft_strchr(set, s1[j - 1]))
         j--;
-    a = malloc(sizeof(char) * (j - i + 2)); // El + 2 se hace para reservarle un espacio al nulo
+    a = malloc(sizeof(char) * (j - i + 1)); // El + 2 se hace para reservarle un espacio al nulo
     if (!a)
         return (0);
-    while (i <= j)
+    while (i < j)
     {
       a[k] = s1[i];
       i++;
