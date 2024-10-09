b84db283c048c98883a200fb6ff3b646d3fd0b0b
mruiz-ur <espartanmanu1122@gmail.com>
2024-10-09 19:30:32 +0200

    updated makefile and libft

--- libft.h
+++ libft.h
@@ -49,5 +49,7 @@ char *ft_substr(char const *s, unsigned int start, size_t len);
 int ft_tolower(int c);
 int ft_toupper(int c);
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void ft_striteri(char *s, void (*f)(unsigned int, char*));
+
 
 #endif
