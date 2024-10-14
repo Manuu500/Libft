0d015b3b615dfb74c3466d5802605d63523c6d2d
Manuu500 <espartanmanu1122@gmail.com>
2024-10-14 14:58:02 +0200

    progressed on split function

--- ft_split.c
+++ ft_split.c
@@ -14,15 +14,49 @@
 
 char **ft_split(char const *s, char c)
 {
-	size_t	i;
 	char	*str;
+	size_t	str_long;
+
+	str = NULL;
+	str_long = word_count(s,c);
+	str = malloc(sizeof(char) * (str_long + 1));
+	if (!str)
+	{
+		m_free(str);
+		return (0);
+	}
+	
+}
+
+char	word_count(char const *s, char c)
+{
+	size_t	i;
 
 	i = 0;
+	while (i < c)
+		i++;
+	return (i);
+}
+
+void	m_free (char *str)
+{
+	if (str)
+		free(str);
+}
+
+char	*word_fill (char *s)
+{
+	char	*str;
+	size_t	i;
+
+	str = NULL;
 	str = malloc(sizeof(char) * (ft_strlen(s) + 1));
 	while (s[i] != '\0')
 	{
+		str[i] = s[i];
 		i++;
 	}
+	return (str);
 }
 
 //Meter toda la cadena en un malloc
